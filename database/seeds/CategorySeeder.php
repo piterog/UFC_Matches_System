76dfdcfb7f4a9868a0f2952773ea4d94b4cdcc11
Piter Ortiz <piter_og@hotmail.com>
2020-08-26 22:41:25 -0300

    Fighter and Category seeders

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -11,6 +11,22 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $list = [
+            ['description' => 'STRAWWEIGHT', 'weight' => '115', 'genre' => 'F'],
+            ['description' => 'FLYWEIGHT', 'weight' => '125', 'genre' => 'F'],
+            ['description' => 'BANTAMWEIGHT', 'weight' => '135', 'genre' => 'F'],
+            ['description' => 'FLYWEIGHT', 'weight' => '125', 'genre' => 'M'],
+            ['description' => 'BANTAMWEIGHT', 'weight' => '135', 'genre' => 'M'],
+            ['description' => 'FEATHERWEIGHT', 'weight' => '145', 'genre' => 'M'],
+            ['description' => 'LIGHTWEIGHT', 'weight' => '155', 'genre' => 'M'],
+            ['description' => 'WELTERWEIGHT', 'weight' => '170', 'genre' => 'M'],
+            ['description' => 'MIDDLEWEIGHT', 'weight' => '185', 'genre' => 'M'],
+            ['description' => 'LIGHT HEAVYWEIGHT', 'weight' => '205', 'genre' => 'M'],
+            ['description' => 'HEAVYWEIGHT', 'weight' => '265', 'genre' => 'M'],
+        ];
+
+        foreach($list as $item){
+            \App\Category::create($item);
+        }
     }
 }
